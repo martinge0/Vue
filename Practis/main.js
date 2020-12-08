fe9421716a3f5b6fe369577005db8eb113db41c2
Martin Georgiev <pchelpbs@gmail.com>
2020-12-08 18:08:39 +0200

    v-for

--- Practis/main.js
+++ Practis/main.js
@@ -5,6 +5,17 @@ var app = new Vue({
     image: "./assets/vmSocks-green-onWhite.jpg",
     // url: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks"
     inStock: true,
-    onSale: true
+    onSale: true,
+    details: ["80 % Coton", "20% Poliester", "Gender neutral"],
+    variants: [
+      {
+        variantId:234,
+        variantColor:"green"
+      },
+      {
+        variantId:235,
+        variantColor:"blue"
+      }
+    ]
   },
 });
