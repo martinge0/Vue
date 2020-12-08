264904844513079e67c0dd8308b373663c7a23a9
Martin Georgiev <pchelpbs@gmail.com>
2020-12-08 17:40:14 +0200

    v-if  and v-else

--- Practis/main.js
+++ Practis/main.js
@@ -3,6 +3,8 @@ var app = new Vue({
   data: {
     product: "Socs",
     image: "./assets/vmSocks-green-onWhite.jpg",
-    url: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks"
+    // url: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks"
+    inStock: true,
+    onSale: true
   },
 });
