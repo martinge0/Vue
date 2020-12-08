9ab1aca525722db1d635a232c24c515ed35d6271
Martin Georgiev <pchelpbs@gmail.com>
2020-12-08 13:14:08 +0200

    v-bind

--- Practis/main.js
+++ Practis/main.js
@@ -1,7 +1,8 @@
 var app = new Vue({
-    el : '#app',
-    data : {
-        product: 'Socs',
-        description: 'Very warm socs'
-    }
-})
+  el: "#app",
+  data: {
+    product: "Socs",
+    image: "./assets/vmSocks-green-onWhite.jpg",
+    url: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks"
+  },
+});
